d191290756a39a96b9447044405750cf25a1cac7
Justin Schneck <jschneck@mac.com>
2016-01-05 22:07:30 -0500

    simplify adapters

--- lib/bake/adapter.ex
+++ lib/bake/adapter.ex
@@ -1,3 +1,5 @@
 defmodule Bake.Adapter do
-  
+  use Behaviour
+  defcallback firmware(config :: Bake.Config.t, target :: Atom.t, otp_name :: String.t) :: nil
+  defcallback burn(config :: Bake.Config.t, target :: Atom.t, otp_name :: String.t) :: nil
 end
