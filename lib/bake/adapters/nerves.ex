db1f504d3022f7a8acff95e46fddb1052377d98e
Justin Schneck <jschneck@mac.com>
2016-01-07 19:32:59 -0500

    Updated the firmware compiler to clean the deps if the target changes from build to build

--- lib/bake/adapters/nerves.ex
+++ lib/bake/adapters/nerves.ex
@@ -1,4 +1,5 @@
 defmodule Bake.Adapters.Nerves do
+  import BakeUtils.Cli.Config, only: [encode_term: 1, decode_term: 1, decode_elixir: 1]
   @behaviour Bake.Adapter
 
   @nerves_home System.get_env("NERVES_HOME") || "~/.nerves"
@@ -10,6 +11,81 @@ defmodule Bake.Adapters.Nerves do
 
   def firmware(config, target, otp_name) do
     Bake.Shell.info "=> Building firmware for target #{target}"
+    {toolchain_path, system_path} = config_env(config, target)
+    rel2fw = "#{system_path}/scripts/rel2fw.sh"
+    stream = IO.binstream(:standard_io, :line)
+    env = [
+      {"NERVES_APP", File.cwd!},
+      {"NERVES_TOOLCHAIN", toolchain_path},
+      {"NERVES_SYSTEM", system_path},
+      {"NERVES_TARGET", to_string(target)},
+      {"MIX_ENV", System.get_env("MIX_ENV") || "dev"}
+    ]
+
+    cmd = """
+    source nerves-env.sh &&
+    cd #{File.cwd!} &&
+    """
+
+    #check for the env cache
+    if File.dir?("#{File.cwd!}/_build") do
+      # Load the env file
+      case File.read("#{File.cwd!}/_build/nerves_env") do
+        {:ok, file} ->
+          build_env =
+          case decode_term(file) do
+            {:ok, term} -> term
+            {:error, _} -> decode_elixir(file)
+          end
+          unless build_env["NERVES_TARGET"] == to_string(target) do
+            cmd = cmd <> """
+            mix deps.clean --all &&
+            mix deps.get &&
+            mix deps.compile &&
+            """
+          end
+        _ -> nil
+      end
+    else
+
+    end
+
+    cmd = cmd <> """
+    mix compile &&
+    mix release &&
+    sh #{rel2fw} rel/#{otp_name} _images/#{otp_name}-#{target}.fw
+    """ |> remove_newlines
+
+
+    Porcelain.shell(cmd, dir: system_path, env: env, out: stream)
+
+    if File.dir?("#{File.cwd!}/_build") do
+      result = File.write("#{File.cwd!}/_build/nerves_env", encode_term(env))
+      Logger.debug "Result"
+    end
+
+  end
+
+  def burn(config, target, otp_name) do
+    Bake.Shell.info "=> Burning firmware for target #{target}"
+    {toolchain_path, system_path} = config_env(config, target)
+    stream = IO.binstream(:standard_io, :line)
+    env = [
+      {"NERVES_APP", File.cwd!},
+      {"NERVES_TOOLCHAIN", toolchain_path},
+      {"NERVES_SYSTEM", system_path},
+      {"MIX_ENV", System.get_env("MIX_ENV") || "dev"}
+    ]
+    fw = "_images/#{otp_name}-#{target}.fw"
+    cmd = "fwup -a -i #{fw} -t complete"
+    Porcelain.shell(cmd, env: env, out: stream)
+  end
+
+  defp remove_newlines(string) do
+    string |> String.strip |> String.replace("\n", " ")
+  end
+
+  defp config_env(config, target) do
     target_atom =
     cond do
       is_atom(target) -> target
@@ -34,8 +110,6 @@ defmodule Bake.Adapters.Nerves do
     {:ok, system_config} = "#{system_path}/recipe.exs"
     |> Bake.Config.Recipe.read!
 
-    rel2fw = "#{system_path}/scripts/rel2fw.sh"
-
     #Logger.debug "System Config: #{inspect system_config}"
     # Toolchain
     {username, toolchain_tuple, _toolchain_version} = system_config[:toolchain]
@@ -51,28 +125,6 @@ defmodule Bake.Adapters.Nerves do
     else
       raise "Toolchain #{username}-#{toolchain_tuple}-#{host_platform}-#{host_arch} not downloaded"
     end
-
-    stream = IO.binstream(:standard_io, :line)
-    env = [
-      {"NERVES_APP", File.cwd!},
-      {"NERVES_TOOLCHAIN", toolchain_path},
-      {"NERVES_SYSTEM", system_path},
-      {"MIX_ENV", System.get_env("MIX_ENV") || "dev"}
-    ]
-    cmd = """
-    source nerves-env.sh &&
-    cd #{File.cwd!} &&
-    mix release &&
-    sh #{rel2fw} rel/#{otp_name} _images/#{otp_name}-#{target}.fw
-    """ |> remove_newlines
-    Porcelain.shell(cmd, dir: system_path, env: env, out: stream)
-  end
-
-  def burn(config, target, otp_name) do
-
-  end
-
-  defp remove_newlines(string) do
-    string |> String.strip |> String.replace("\n", " ")
+    {toolchain_path, system_path}
   end
 end
