a30ea334513c48360d6a9937ae8db5e0434995fe
Justin Schneck <jschneck@mac.com>
2016-01-10 20:43:14 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Bake.Mixfile do
 
   def project do
     [app: :bake,
-     version: "0.0.4",
+     version: "0.0.5-dev",
      elixir: "~> 1.1",
      escript: [main_module: Bake.Cli, name: "bake", path: "/usr/local/bin/bake"],
      build_embedded: Mix.env == :prod,
