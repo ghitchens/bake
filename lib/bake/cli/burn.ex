e9b7a3b98f5222b75fb64d7a56dfcacf5b43edcb
Justin Schneck <jschneck@mac.com>
2016-01-15 20:26:39 -0500

    added bake burn

--- lib/bake/cli/burn.ex
+++ lib/bake/cli/burn.ex
@@ -1,7 +1,7 @@
 defmodule Bake.Cli.Burn do
   use Bake.Cli.Menu
 
-  @switches []
+  @switches [target: :string, bakefile: :string]
 
   defp menu do
     """
@@ -9,9 +9,34 @@ defmodule Bake.Cli.Burn do
   end
 
   def main(args) do
-    Bake.start
     {opts, _, _} = OptionParser.parse(args, switches: @switches)
-    Bake.Shell.info "Burn Firmware"
+    if opts[:target] == nil, do: raise """
+      You must specify a target to burn
+    """
+
+    target = opts[:target]
+    bakefile = opts[:bakefile] || System.cwd! <> "/Bakefile"
+
+    case Bake.Config.read!(bakefile) do
+      {:ok, config} ->
+        case Bake.Config.filter_target(config, target) do
+          [] -> Bake.Shell.info "Bakefile does not contain definition for target #{target}"
+          target_config ->
+            platform = target_config[:platform]
+            |> Atom.to_string
+            |> String.capitalize
+
+            mod = Module.concat(Bake.Adapters, platform)
+
+            otp_name = Path.dirname(bakefile) |> String.split("/") |> List.last
+            #Enum.each(target_config[:target], fn({target, v}) ->
+              mod.burn(target_config, target, otp_name)
+            #end)
+
+        end
+      {:error, e} ->
+        Bake.Shell.info "No Bakefile Found"
+    end
   end
 
 end
